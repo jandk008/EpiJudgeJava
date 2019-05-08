0b46ae33b49225903f64f5a89279a5d310c41929
zczhou10 <zczhou10@gmail.com>
2019-05-09 00:25:27 +0900

    Render a calender for maximum concurrent events

--- epi_judge_java/epi/CalendarRendering.java
+++ epi_judge_java/epi/CalendarRendering.java
@@ -2,7 +2,12 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import sun.rmi.server.LoaderHandler;
+
 public class CalendarRendering {
   @EpiUserType(ctorParams = {int.class, int.class})
 
@@ -15,7 +20,7 @@ public class CalendarRendering {
     }
   }
 
-  private static class Endpoint {
+  private static class Endpoint implements Comparable<Endpoint>{
     public int time;
     public boolean isStart;
 
@@ -23,13 +28,37 @@ public class CalendarRendering {
       this.time = time;
       this.isStart = isStart;
     }
+
+    @Override
+    public int compareTo(final Endpoint o) {
+      if (time != o.time) {
+        return Integer.compare(time, o.time);
+      }
+      return isStart && !o.isStart ? -1 : !isStart && o.isStart ? 1 : 0;
+    }
   }
 
   @EpiTest(testDataFile = "calendar_rendering.tsv")
 
   public static int findMaxSimultaneousEvents(List<Event> A) {
-    // TODO - you fill in here.
-    return 0;
+    List<Endpoint> endpoints = new ArrayList<>();
+    for(Event event : A) {
+      endpoints.add(new Endpoint(event.start, true));
+      endpoints.add(new Endpoint(event.finish, false));
+    }
+    Collections.sort(endpoints);
+
+    int max = 0;
+    int local = 0;
+    for(Endpoint endpoint : endpoints) {
+      if (endpoint.isStart) {
+        local++;
+        max = Math.max(max, local);
+      } else {
+        local--;
+      }
+    }
+    return max;
   }
 
   public static void main(String[] args) {
