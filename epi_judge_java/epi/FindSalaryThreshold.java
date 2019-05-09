30a23c95f7844331aa8fd0cbc681e5a12f7ba16f
jandk008 <zczhou10@gmail.com>
2019-05-09 19:35:02 +0900

    Compute salary cap/threshold
    **problem**
    Design an algorithm for computing the salary cap, given existing salaries and the target payroll.
    For example, if there were five employees with salaries last year were $90,$30,$100,$40, and $20, and the target payroll this year is $210, then 60 is a suitable salary cap, since 60 + 30 + 60 + 40 + 20 = 210.

--- epi_judge_java/epi/FindSalaryThreshold.java
+++ epi_judge_java/epi/FindSalaryThreshold.java
@@ -1,14 +1,27 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import java.util.List;
-public class FindSalaryThreshold {
-  @EpiTest(testDataFile = "find_salary_threshold.tsv")
 
-  public static double findSalaryCap(int targetPayroll,
-                                     List<Integer> currentSalaries) {
+public class FindSalaryThreshold {
+
+  @EpiTest(testDataFile = "find_salary_threshold.tsv")
+  public static double findSalaryCap(int targetPayroll, List<Integer> currentSalaries) {
     // TODO - you fill in here.
-    return 0.0;
+    currentSalaries.sort(Integer::compare);
+
+    double threshold = targetPayroll / (1.0 * currentSalaries.size());
+    int sum = 0;
+    for(int i = 0; i < currentSalaries.size(); i++) {
+      int integer = currentSalaries.get(i);
+      if (threshold - integer < 0) {
+        threshold = (targetPayroll - sum) / (1.0 * (currentSalaries.size() - i));
+        if (threshold < integer) return threshold;
+      }
+      sum += integer;
+    }
+    return targetPayroll > sum ? -1 : threshold;
   }
 
   public static void main(String[] args) {
