7b9f9cc29214f51ee277a1d3381358313173a2c5
Zhou, Zicheng <zichengz@amazon.co.jp>
2019-05-09 17:55:02 +0900

    Sort Linked ListNode using merge sort
    **problem**
    Implement a routine which sorts lists efficiently. It should be a stable sort, i.e., the relative positions of equal elements must remain unchanged

--- epi_judge_java/epi/SortList.java
+++ epi_judge_java/epi/SortList.java
@@ -1,12 +1,26 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class SortList {
-  @EpiTest(testDataFile = "sort_list.tsv")
 
+public class SortList {
+
+  @EpiTest(testDataFile = "sort_list.tsv")
   public static ListNode<Integer> stableSortList(ListNode<Integer> L) {
     // TODO - you fill in here.
-    return null;
+    if (L == null || L.next == null) return L;
+
+    ListNode<Integer> slow = L;
+    ListNode<Integer> fast = L;
+    ListNode<Integer> preSlow = slow;
+    while (fast != null && fast.next != null) {
+      preSlow = slow;
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    preSlow.next = null;
+
+    return SortedListsMerge.mergeTwoSortedLists(stableSortList(L), stableSortList(slow));
   }
 
   public static void main(String[] args) {
--- epi_judge_java/epi/SortedListsMerge.java
+++ epi_judge_java/epi/SortedListsMerge.java
@@ -1,13 +1,28 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class SortedListsMerge {
+
   @EpiTest(testDataFile = "sorted_lists_merge.tsv")
-  //@include
-  public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1,
-                                                      ListNode<Integer> L2) {
+  public static ListNode<Integer> mergeTwoSortedLists(ListNode<Integer> L1, ListNode<Integer> L2) {
     // TODO - you fill in here.
-    return null;
+    ListNode<Integer> head = new ListNode<>(0, null);
+    ListNode<Integer> current = head;
+    while(L1 != null && L2 != null) {
+      if (L1.data <= L2.data) {
+        current.next = L1;
+        L1 = L1.next;
+      } else {
+        current.next = L2;
+        L2 = L2.next;
+      }
+      current = current.next;
+    }
+    if (L1 != null) current.next = L1;
+    else current.next = L2;
+    return head.next;
   }
 
   public static void main(String[] args) {
