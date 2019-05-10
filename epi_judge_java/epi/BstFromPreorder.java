275f8ac1257a724069d458ba05083142aefdc8ac
jandk008 <zczhou10@gmail.com>
2019-05-10 15:49:50 +0900

    Rebuild binary search tree from preorder sequence

--- epi_judge_java/epi/BstFromPreorder.java
+++ epi_judge_java/epi/BstFromPreorder.java
@@ -3,12 +3,23 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import java.util.List;
 public class BstFromPreorder {
-  @EpiTest(testDataFile = "bst_from_preorder.tsv")
 
-  public static BstNode<Integer>
-  rebuildBSTFromPreorder(List<Integer> preorderSequence) {
+  private static int index;
+  @EpiTest(testDataFile = "bst_from_preorder.tsv")
+  public static BstNode<Integer> rebuildBSTFromPreorder(List<Integer> preorderSequence) {
     // TODO - you fill in here.
-    return null;
+    index = 0;
+    return constructBstFromPreorderOnValueRange(preorderSequence, Integer.MIN_VALUE, Integer.MAX_VALUE);
+  }
+
+  private static BstNode<Integer> constructBstFromPreorderOnValueRange(List<Integer> preorder, int low, int high) {
+    if (index >= preorder.size()) return null;
+    int root = preorder.get(index);
+    if (root > high || root < low) return null;
+    index++;
+    BstNode<Integer> left = constructBstFromPreorderOnValueRange(preorder, low, root);
+    BstNode<Integer> right = constructBstFromPreorderOnValueRange(preorder, root, high);
+    return new BstNode<>(root, left, right);
   }
 
   public static void main(String[] args) {
