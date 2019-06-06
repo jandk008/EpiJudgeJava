d0dc8c956db9f22b61dc961eb3c5a66d7177ddb7
jandk008 <zczhou10@gmail.com>
2019-06-06 10:52:46 +0900

    Compute longest substring with matching parentheses
    **Problem**
    Write a program that takes as input a string made up of the characters '(' and ')', and returns the size of a maximum length substring in which the parens are matched

--- epi_judge_java/epi/LongestSubstringWithMatchingParentheses.java
+++ epi_judge_java/epi/LongestSubstringWithMatchingParentheses.java
@@ -6,7 +6,26 @@ public class LongestSubstringWithMatchingParentheses {
 
   public static int longestMatchingParentheses(String s) {
     // TODO - you fill in here.
-    return 0;
+    if (s == null || s.length() < 2) return 0;
+    int max = 0;
+    char[] cs = s.toCharArray();
+    for (int i = 0; i < cs.length; i++) {
+      if (cs[i] == ')') continue;
+      int numOpen = 1;
+      for (int j = i + 1; j < cs.length; j++) {
+        if (cs[j] == ')') {
+            if (numOpen == 1) {
+              max = Math.max(max, j - i + 1);
+            } else if (numOpen < 1){
+              break;
+            }
+          numOpen --;
+        } else {
+          numOpen ++;
+        }
+      }
+    }
+    return max;
   }
 
   public static void main(String[] args) {
