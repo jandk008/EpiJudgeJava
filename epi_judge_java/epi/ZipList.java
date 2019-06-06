74e86c463d0501fe8fd8b0e255ad8391e5fb69b4
jandk008 <zczhou10@gmail.com>
2019-06-06 10:54:04 +0900

    Implement list zipping
    **Problem**
    LetLbe a singly linked list. Assume its nodes are numbered starting at 0. Define the zip of L to be the list consisting of the interleaving of the nodes numbered 0,1, 2, . . . with the nodes numbered n-1,n-2,n-3,.. where n is the number of nodes in the list. The zipping function is illustrated in Figure 4.1 on Page 27.
    Implement the zip function.

--- epi_judge_java/epi/ZipList.java
+++ epi_judge_java/epi/ZipList.java
@@ -6,7 +6,44 @@ public class ZipList {
 
   public static ListNode<Integer> zippingLinkedList(ListNode<Integer> L) {
     // TODO - you fill in here.
-    return null;
+    if (L == null || L.size() < 3) return L;
+
+    ListNode<Integer> slow = L;
+    ListNode<Integer> fast = L;
+    while(fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    ListNode<Integer> secondHead = reverse(slow.next);
+    slow.next = null;
+
+    ListNode<Integer> iterator1 = L;
+    ListNode<Integer> iterator2 = secondHead;
+
+    while(iterator2 != null) {
+        ListNode<Integer> t = iterator2.next;
+        iterator2.next = iterator1.next;
+        iterator1.next = iterator2;
+        iterator1 = iterator1.next.next;
+        iterator2 = t;
+    }
+
+    return L;
+  }
+
+  private static ListNode<Integer> reverse(ListNode<Integer> head) {
+    if (head  == null || head.next == null) return head;
+    ListNode<Integer> dummy = new ListNode<>(0, head);
+    ListNode<Integer> current = head.next;
+    head.next = null;
+    while (current != null) {
+      ListNode<Integer> t = current.next;
+      current.next = dummy.next;
+      dummy.next = current;
+      current = t;
+    }
+    return dummy.next;
   }
 
   public static void main(String[] args) {
