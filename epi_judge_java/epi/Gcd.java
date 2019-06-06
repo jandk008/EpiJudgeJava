1c0370d375c639f5a0cd29a384a4b60c10cd1714
jandk008 <zczhou10@gmail.com>
2019-06-06 10:55:26 +0900

    Compute greatest common divisor with bit manipulation

--- epi_judge_java/epi/Gcd.java
+++ epi_judge_java/epi/Gcd.java
@@ -2,11 +2,21 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class Gcd {
-  @EpiTest(testDataFile = "gcd.tsv")
 
+  @EpiTest(testDataFile = "gcd.tsv")
   public static long GCD(long x, long y) {
     // TODO - you fill in here.
-    return 0;
+    if (x == 0 || y == 0) return x == 0 ? y : x;
+    if (x == y) return x;
+    if ((x & 1) == 0 && (y & 1) == 0) {
+      return GCD(x >> 1, y >> 1) << 1;
+    } else if ((x & 1) == 1 && (y & 1) == 1) {
+      return x > y ? GCD( x - y, y) : GCD(x, y - x);
+    } else if ((x & 1) == 0){
+      return GCD(x >> 1, y);
+    } else {
+      return GCD(x, y >> 1);
+    }
   }
 
   public static void main(String[] args) {
